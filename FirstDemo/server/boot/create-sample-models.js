c8007eb2d14e030b79ef09163fe9c4c23ee4f8f4
PIYUESH BHATIA <PIYUESH BHATIA>
2016-06-22 01:21:19 +0530

    adding hr portal app

--- FirstDemo/server/boot/create-sample-models.js
+++ FirstDemo/server/boot/create-sample-models.js
@@ -1,79 +1,80 @@
-//U- Add automigration script
-var async = require('async');
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
+// Node module: loopback-example-access-control
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
 module.exports = function(app) {
-  //data sources
-  var mongoDs = app.dataSources.mongoDs;
-  var mysqIDs = app.dataSources.mysqIDs;
-  //create all models
-  async.parallel({
-    reviewers: async.apply(createReviewers),
-    coffeeShops: async.apply(createCoffeeShops),
-  }, function(err, results) {
+  var User = app.models.user;
+  var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
+  var Team = app.models.Team;
+
+  User.create([
+    {username: 'John', email: 'john@doe.com', password: 'opensesame'},
+    {username: 'Jane', email: 'jane@doe.com', password: 'opensesame'},
+    {username: 'Bob', email: 'bob@projects.com', password: 'opensesame'}
+  ], function(err, users) {
     if (err) throw err;
-    createReviews(results.reviewers, results.coffeeShops, function(err) {
-      console.log('> models created sucessfully');
+
+    console.log('Created users:', users);
+
+    // create project 1 and make john the owner
+    users[0].projects.create({
+      name: 'project1',
+      balance: 100
+    }, function(err, project) {
+      if (err) throw err;
+
+      console.log('Created project:', project);
+
+      // add team members
+      Team.create([
+        {ownerId: project.ownerId, memberId: users[0].id},
+        {ownerId: project.ownerId, memberId: users[1].id}
+      ], function(err, team) {
+        if (err) throw err;
+
+        console.log('Created team:', team);
+      });
+    });
+
+    //create project 2 and make jane the owner
+    users[1].projects.create({
+      name: 'project2',
+      balance: 100
+    }, function(err, project) {
+      if (err) throw err;
+
+      console.log('Created project:', project);
+
+      //add team members
+      Team.create({
+        ownerId: project.ownerId,
+        memberId: users[1].id
+      }, function(err, team) {
+        if (err) throw err;
+
+        console.log('Created team:', team);
+      });
+    });
+
+    //create the admin role
+    Role.create({
+      name: 'admin'
+    }, function(err, role) {
+      if (err) throw err;
+
+      console.log('Created role:', role);
+
+      //make bob an admin
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: users[2].id
+      }, function(err, principal) {
+        if (err) throw err;
+
+        console.log('Created principal:', principal);
+      });
     });
   });
-  //create reviewers
-  function createReviewers(cb) {
-    mongoDs.automigrate('Reviewer', function(err) {
-      if (err) return cb(err);
-      var Reviewer = app.models.Reviewer;
-      Reviewer.create([
-        {email: 'foo@bar.com', password: 'foobar'},
-        {email: 'john@doe.com', password: 'johndoe'},
-        {email: 'jane@doe.com', password: 'janedoe'}
-      ], cb);
-    });
-  }
-  //create coffee shops
-  function createCoffeeShops(cb) {
-    mysqIDs.automigrate('CoffeeShop', function(err) {
-      if (err) return cb(err);
-      var CoffeeShop = app.models.CoffeeShop;
-      CoffeeShop.create([
-        {name: 'Bel Cafe', city: 'Vancouver'},
-        {name: 'Three Bees Coffee House', city: 'San Mateo'},
-        {name: 'Caffe Artigiano', city: 'Vancouver'},
-      ], cb);
-    });
-  }
-  //create reviews
-  function createReviews(reviewers, coffeeShops, cb) {
-    mongoDs.automigrate('Review', function(err) {
-      if (err) return cb(err);
-      var Review = app.models.Review;
-      var DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
-      Review.create([
-        {
-          date: Date.now() - (DAY_IN_MILLISECONDS * 4),
-          rating: 5,
-          comments: 'A very good coffee shop.',
-          publisherId: reviewers[0].id,
-          coffeeShopId: coffeeShops[0].id,
-        },
-        {
-          date: Date.now() - (DAY_IN_MILLISECONDS * 3),
-          rating: 5,
-          comments: 'Quite pleasant.',
-          publisherId: reviewers[1].id,
-          coffeeShopId: coffeeShops[0].id,
-        },
-        {
-          date: Date.now() - (DAY_IN_MILLISECONDS * 2),
-          rating: 4,
-          comments: 'It was ok.',
-          publisherId: reviewers[1].id,
-          coffeeShopId: coffeeShops[1].id,
-        },
-        {
-          date: Date.now() - (DAY_IN_MILLISECONDS),
-          rating: 4,
-          comments: 'I go here everyday.',
-          publisherId: reviewers[2].id,
-          coffeeShopId: coffeeShops[2].id,
-        }
-      ], cb);
-    });
-  }
 };
--- FirstDemo/server/server.js
+++ FirstDemo/server/server.js
@@ -1,12 +1,22 @@
-var loopback = require('loopback');
+var bodyParser = require('body-parser');
 var boot = require('loopback-boot');
+var loopback = require('loopback');
+var path = require('path');
 
 var app = module.exports = loopback();
 
-//U-express route add
-app.use('/express-status', function(req, res, next) {
-  res.json({ running: true });
-});
+app.middleware('initial', bodyParser.urlencoded({ extended: true }));
+
+// Bootstrap the application, configure models, datasources and middleware.
+
+boot(app, __dirname);
+
+app.set('view engine', 'ejs'); // LoopBack comes with EJS out-of-box
+app.set('json spaces', 2); // format json responses for easier viewing
+
+// must be set to serve views properly when starting the app via `slc run` from
+// the project root
+app.set('views', path.resolve(__dirname, 'views'));
 
 app.start = function() {
   // start the web server
@@ -21,12 +31,7 @@ app.start = function() {
   });
 };
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
+// start the server if `$ node server.js`
+if (require.main === module) {
+  app.start();
+}
